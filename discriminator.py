8d07542e87c6c3b2a516a0a9a96c4a5014e1b5c0
Takeyuki Watadani <watadat-tky@umin.ac.jp>
2018-11-21 17:47:09 +0900

    repair symlink to real file

--- discriminator.py
+++ discriminator.py
@@ -1,1 +1,198 @@
-../dcgan_v4/discriminator.py
+# -*- coding: utf-8 -*-
+
+########## discriminator.py ##########
+#
+# DCGAN Discriminator
+# 
+#
+# created 2018/10/07 Takeyuki Watadani @ UT Radiology
+#
+########################################
+
+import config as cf
+import functions as f
+
+import tensorflow as tf
+import numpy as np
+
+import os.path
+import io
+import math
+import random
+import zipfile
+
+logger = cf.LOGGER
+
+class Discriminator:
+    '''DCGANのDiscriminatorを記述するクラス'''
+
+
+    def __init__(self, global_step):
+        self.global_step_tensor = global_step
+
+        self.from_dataset = None # データセットからの入力画像
+        self.from_generator = None # generatorからの入力画像
+        self.output = None # 出力の確率
+        self.optimizer = None
+        self.train_op = None
+
+        self.input_selector = None # generatorからの時は True
+
+    def define_forward(self, input, vreuse = None):
+        '''判定する計算を返す'''
+        
+        with tf.variable_scope('D_network', reuse=vreuse):
+
+            inreshaped = tf.reshape(input,
+                                    shape = (-1, cf.PIXELSIZE, cf.PIXELSIZE, 1),
+                                    name = 'D_inreshaped')
+
+            c1 = 'D_conv1'
+            conv1 = f.apply_dobn(tf.layers.conv2d(inputs = inreshaped,
+                                                  filters = 1,
+                                                  kernel_size = (4, 4),
+                                                  strides = (2, 2),
+                                                  padding = 'same',
+                                                  kernel_initializer = tf.keras.initializers.he_normal(),
+                                                  activation = tf.nn.leaky_relu,
+                                                  name = c1),
+                                 c1)
+            f.print_shape(conv1)
+
+            c2 = 'D_conv2'
+            conv2 = f.apply_dobn(tf.layers.conv2d(inputs = conv1,
+                                                  filters = 64,
+                                                  kernel_size = (4, 4),
+                                                  strides = (2, 2),
+                                                  padding = 'same',
+                                                  kernel_initializer = tf.keras.initializers.he_normal(),
+                                                  activation = tf.nn.leaky_relu,
+                                                  name = c2),
+                                 c2)
+            f.print_shape(conv2)
+
+            c3 = 'D_conv3'
+            conv3 = f.apply_dobn(tf.layers.conv2d(inputs = conv2,
+                                                  filters = 128,
+                                                  kernel_size = (4, 4),
+                                                  strides = (2, 2),
+                                                  padding = 'same',
+                                                  kernel_initializer = tf.keras.initializers.he_normal(),
+                                                  activation = tf.nn.leaky_relu,
+                                                  name = c3),
+                                 c3)
+            f.print_shape(conv3)
+
+            c4 = 'D_conv4'
+            conv4 = f.apply_dobn(tf.layers.conv2d(inputs = conv3,
+                                                  filters = 256,
+                                                  kernel_size = (4, 4),
+                                                  strides = (2, 2),
+                                                  padding = 'same',
+                                                  kernel_initializer = tf.keras.initializers.he_normal(),
+                                                  activation = tf.nn.leaky_relu,
+                                                  name = c4),
+                                 c4)
+            f.print_shape(conv4)
+
+            flatten = tf.layers.flatten(conv4,
+                                        name = 'D_flatten')
+            f.print_shape(flatten)
+
+            #conv4reshaped = tf.reshape(conv4, shape=(-1, 4, 4, 256, 1))
+            #gab = 'D_global_average_pooling'
+            #finallayer = tf.layers.average_pooling3d(inputs = conv4reshaped,
+            #                                         pool_size = (4, 4, 256),
+            #                                         strides = (1, 1, 1),
+            #                                         padding = 'valid',
+            #                                         name = gab)
+                                                     
+            fc = 'D_fully_connected'
+            fully_connected = f.apply_dobn(tf.layers.dense(inputs = flatten,
+                                                           units = 1,
+                                                           kernel_initializer = tf.initializers.ones,
+                                                           activation = tf.nn.sigmoid,
+                                                           name = fc),
+                                           fc)
+            f.print_shape(fully_connected)
+            return fully_connected
+            #return tf.nn.sigmoid(tf.reshape(finallayer, shape=(-1, 1)))
+
+    def define_graph(self):
+        '''discriminatorの計算グラフを定義する'''
+
+        with tf.variable_scope('D_network'):
+
+            self.from_dataset = tf.placeholder(dtype=tf.float32, shape=(None, cf.PIXELSIZE, cf.PIXELSIZE), name='D_input_image')
+        
+            #epsilon = 0.0000001
+            #random1 = tf.random_normal(shape=(cf.MINIBATCHSIZE, 1),
+            #                           mean=0.0,
+            #                           stddev=0.015,
+            #                           dtype=tf.float32,
+            #                           name='random_1')
+            zeros = tf.zeros(shape=(cf.MINIBATCHSIZE, 1),
+                             dtype = tf.float32,
+                             name='D_zeros')
+                                
+            #random2 = tf.random_normal(shape=(cf.MINIBATCHSIZE, 1),
+            #                           mean=0.0,
+            #                           stddev=0.015,
+            #                           dtype=tf.float32,
+            #                           name='random_2')
+            #onesrandom = tf.random_normal(shape=(cf.MINIBATCHSIZE, 1),
+            #                              mean = 1.0,
+            #                              stddev = 0.015,
+            #                              dtype = tf.float32,
+            #                              name='onesrandom')
+            ones = tf.ones(shape=(cf.MINIBATCHSIZE, 1),
+                           dtype = tf.float32,
+                           name='D_ones')
+            
+            #self.p_real = tf.maximum(tf.add(self.define_forward(self.from_dataset,
+            #                                                    vreuse=False), random1),
+            #                         epsilon, name='p_real')
+            self.p_real = self.define_forward(self.from_dataset, vreuse=False)
+            self.p_fake = self.define_forward(self.from_generator, vreuse=True)
+            #self.p_fake_for_loss = tf.maximum(tf.add(self.p_fake, random2), epsilon,
+            #                                  name='p_fake_for_loss')
+
+            G_crossentropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=zeros, logits=self.p_fake)
+            D_crossentropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=ones, logits=self.p_real)
+
+
+            self.loss = tf.reduce_mean(tf.add(D_crossentropy, G_crossentropy), name='D_loss')
+            #self.loss = -tf.reduce_mean(tf.log(0.0001 + self.p_real) - tf.log(0.0001 + self.p_fake_for_loss), name='D_loss')
+            tf.summary.scalar(name = 'Discriminator loss', tensor = self.loss)
+            D_vars = [x for x in tf.trainable_variables() if 'D_' in x.name]
+            
+            self.optimizer = tf.train.AdamOptimizer(learning_rate = cf.D_LEARNING_RATE,
+                                                    beta1 = cf.BETA_1,
+                                                    name = 'D_optimizer')
+            self.train_op = self.optimizer.minimize(self.loss,
+                                                    global_step=self.global_step_tensor,
+                                                    var_list=D_vars,
+                                                    name='D_train_op')
+
+    def set_input_from_generator(self, generator):
+        with tf.variable_scope('D_network'):
+            self.from_generator = generator.output
+        return
+
+    @staticmethod
+    def create_minibatch():
+        '''データセットからミニバッチを作成する'''
+        zippath = os.path.join(cf.DATASET_PATH, cf.TRAIN_PREFIX + '.zip')
+        with zipfile.ZipFile(zippath, 'r') as zf:
+
+            nplist = zf.namelist()
+            npsampled = random.sample(nplist, cf.MINIBATCHSIZE)
+
+            minibatch = []
+
+            for i in range(cf.MINIBATCHSIZE):
+                bytes = zf.read(npsampled[i])
+                buf = io.BytesIO(bytes)
+                minibatch.append(np.load(buf))
+        return minibatch
+        
--- train.py
+++ train.py
@@ -1,1 +1,327 @@
-../dcgan_v4/train.py
+# -*- coding: utf-8 -*-
+
+########## train.py ##########
+#
+#
+# DCGAN
+# ネットワークの学習を行うメインプログラム
+#
+# created 2018/10/07 Takeyuki Watadani
+#
+########################################
+
+import config as cf
+import functions as f
+import discriminator as d
+import generator as g
+
+import random
+import queue
+import os.path
+import math
+import traceback
+from time import sleep
+from concurrent.futures import ThreadPoolExecutor
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+logger = cf.LOGGER
+
+class Trainer:
+    '''GANの学習を行うためのクラス'''
+
+    def __init__(self, global_step, discriminator, generator):
+        self.D = discriminator
+        self.G = generator
+        self.trainq = queue.Queue(maxsize = cf.QUEUEMAX)
+        self.training = False
+        self.global_step_tensor = global_step
+        self.session = None
+        self.chkpdir = os.path.join(cf.RESULTDIR, cf.DESC)
+        self.saver = tf.train.Saver()
+        self.tf_initialize()
+
+
+    def tf_initialize(self):
+        '''TensorFlow関係の初期化'''
+        tf.global_variables_initializer()
+
+    def train(self, nqueuethreads=2):
+        '''学習を行うメインルーチン
+        ここからqueuingスレッドと学習を行う本スレッドに分岐する'''
+
+        self.training = True
+        try:
+            executor = ThreadPoolExecutor()
+
+            qfutures = []
+            for i in range(nqueuethreads):
+                qfutures.append(executor.submit(self.queuing_loop, i))
+
+                #D, Gの計算グラフを定義
+                #self.D.set_input_from_generator(self.G)
+            self.G.define_graph()
+            self.D.define_graph()
+            self.G.define_graph_postD()
+
+            #tf.global_variables_initializer()
+            #tf.local_variables_initializer()
+            #D_vars = [x for x in tf.trainable_variables() if 'D_' in x.name]
+            #G_vars = [x for x in tf.trainable_variables() if 'G_' in x.name]
+            #varlist = D_vars + G_vars
+            #self.var_initialize_op = tf.variables_initializer(varlist)
+
+            #self.summarywriter = tf.contrib.summary.create_file_writer(logdir=self.chkpdir)
+            ###self.summarywriter.set_as_default()
+
+            self.train_loop()
+        except:
+            import traceback
+            traceback.print_exc()
+        self.training = False
+        return
+
+    def obtain_minibatch(self):
+        '''学習用のキューからミニバッチデータを取り出す'''
+        while self.trainq.empty():
+            sleep(cf.SLEEP_INTERVAL)
+        return self.trainq.get()
+
+    def queuing_loop(self, qloopid):
+        '''ミニバッチを作成し、キューに格納する処理の本体'''
+    
+        try:
+            logger.info('queuing_loopを開始します。id=' + str(qloopid))
+            if self.trainq is None:
+                self.trainq = queue.Queue(maxsize=cf.QUEUEMAX)
+
+            while self.training:
+                if self.trainq.qsize() < cf.QUEUEBUFSIZE:
+                    for _ in range(cf.QUEUEBATCHSIZE):
+                        minibatch = self.D.create_minibatch()
+                        self.trainq.put(minibatch)
+                else:
+                    sleep(cf.SLEEP_INTERVAL)
+        except:
+            import traceback
+            traceback.print_exc()
+        logger.info('queuing_loopを終了します。qloopid = ' + str(qloopid))
+
+    def save_sample_img(self, session, feed_dict, total_img, save_generator_img = True):
+        '''Generatorが産生するサンプルイメージを保存する'''
+
+        if save_generator_img:
+            output_batch = session.run(self.G.output, feed_dict=feed_dict)
+        else:
+            output_batch = feed_dict[self.D.from_dataset]
+
+
+        # 新しいImageを作る
+        sampleimg = Image.new(mode='L', size=(cf.PIXELSIZE * 3, cf.PIXELSIZE * 2))
+
+        for i in range(6):
+            if cf.MINIBATCHSIZE >= i:
+                img_np = output_batch[i].reshape((cf.PIXELSIZE, cf.PIXELSIZE))
+                img_uint8np = np.uint8(img_np)
+                img_img = Image.fromarray(img_uint8np, mode='L')
+
+                sampleimg.paste(img_img, box=(cf.PIXELSIZE * (i % 3), cf.PIXELSIZE * (i // 3)))
+
+        #save
+        imgdir = os.path.join(cf.RESULTDIR, cf.DESC)
+        filename = os.path.join(imgdir, 'sample-' + str(total_img) + '.png')
+        sampleimg.save(filename)
+                    
+
+    def train_loop(self):
+        '''学習を行うメインループを記述'''
+        #chkpdir = os.path.join(cf.RESULTDIR, cf.DESC)
+        last_Dscore = 0.5
+        #intensive_trained = False
+
+        #dummy_Dinput = np.zeros(shape=[cf.MINIBATCHSIZE, cf.PIXELSIZE, cf.PIXELSIZE],
+        #                        dtype=np.float)
+
+        try:
+            #with tf.train.MonitoredTrainingSession(checkpoint_dir = chkpdir) as self.session:
+
+            merged = tf.summary.merge_all()
+
+            with tf.Session() as self.session:
+
+                self.summarywriter = tf.summary.FileWriter(self.chkpdir,
+                                                           graph=self.session.graph)
+                                                           # r1.8ではsessionなし
+                                                           #,
+                                                           #session=self.session)
+
+                self.session.run([tf.global_variables_initializer(),
+                                  tf.local_variables_initializer()])
+
+                # feed_dictの準備
+                dminibatch = self.obtain_minibatch()
+
+                gminibatch = []
+                for _ in range(cf.MINIBATCHSIZE):
+                    gminibatch.append(f.generate_latent_vector())
+
+                gminibatch = np.concatenate(gminibatch, axis=0)
+
+                fd = {
+                    self.D.from_dataset: dminibatch,
+                    self.G.latent: gminibatch,
+                }
+
+                #prelosstarget = math.sqrt(1000 * cf.PIXELSIZE * cf.PIXELSIZE)
+                #logger.info('Generatorのpretrainを開始します 目標値 = ' + str(prelosstarget))
+
+                #gpretrainloss = 999999999999
+
+               # while not gpretrainloss < prelosstarget:
+               #     gminibatch = []
+               #     for _ in range(cf.MINIBATCHSIZE):
+               #         gminibatch.append(f.generate_latent_vector())#
+
+        #gminibatch = np.concatenate(gminibatch, axis=0)
+         #           fd = { self.G.latent: gminibatch }
+
+          #          _, pretrain_step, gpretrainloss = self.session.run([self.G.pretrain_op, self.G.global_step_tensor, self.G.pretrain_loss], feed_dict=fd)
+           #         if pretrain_step % 20 == 0:
+            #            logger.info('Step: ' + str(pretrain_step) + ', pretrain loss: ' + str(gpretrainloss))
+
+
+            #logger.info('Generatorのpretrainが終了しました。これより敵対的学習に入ります。')
+
+                #logger.info('Global Stepをリセットします。')
+                #['self.session.run(self.G.reset_global_step_op)
+
+                finished = False
+                while not finished:
+                    
+
+                    # feed_dictの準備
+                    dminibatch = self.obtain_minibatch()
+
+                    gminibatch = []
+                    for _ in range(cf.MINIBATCHSIZE):
+                        gminibatch.append(f.generate_latent_vector())
+
+                    gminibatch = np.concatenate(gminibatch, axis=0)
+
+                    fd = {
+                        self.D.from_dataset: dminibatch,
+                        self.G.latent: gminibatch,
+                    }
+
+                    gstep = self.session.run(self.global_step_tensor, feed_dict=fd) // 2
+
+                    # Discriminatorの学習
+
+                    _, last_Dscore, dloss = self.session.run([self.D.train_op,
+                                                              self.G.mean_D_score, self.D.loss],
+                                                             feed_dict=fd)
+                
+                    # Generatorの学習
+                    _, last_Dscore, gloss = self.session.run([self.G.train_op,
+                                                              self.G.mean_D_score,
+                                                              self.G.loss],
+                                                             feed_dict=fd)
+
+                    if last_Dscore < 1e-1 and cf.USE_INTENSIVE_TRAINING:
+                        counter = 0
+                        while last_Dscore < 0.45 and counter < 50000:
+                            gminibatch = []
+                            for _ in range(cf.MINIBATCHSIZE):
+                               gminibatch.append(f.generate_latent_vector())
+                       
+                            gminibatch = np.concatenate(gminibatch, axis=0)
+                            fd = { self.D.from_dataset: dminibatch, self.G.latent: gminibatch }
+                            _, last_Dscore, gloss, dloss = self.session.run([self.G.train_op,
+                                                                             self.G.mean_D_score,
+                                                                             self.G.loss,
+                                                                             self.D.loss],
+                                                                            feed_dict=fd)
+                            counter += 1
+                            if counter % 10 == 0:
+                                print('counter: ', counter, 'gloss:', gloss,
+                                      'dloss:', dloss, 'last_Dscore:', last_Dscore)
+                    elif last_Dscore > 0.999 and cf.USE_INTENSIVE_TRAINING:
+                        counter = 0
+                        while last_Dscore > 0.70 and counter < 500:
+                            dminibatch = self.obtain_minibatch
+                            fd = { self.D.from_dataset: dminibatch,
+                                   self.G.latent: gminibatch }
+                            _, last_Dscore, dloss = self.session.run([self.D.train_op,
+                                                                      self.G.mean_D_score,
+                                                                      self.D.loss],
+                                                                     feed_dict=fd)
+                            counter += 1
+                            if counter % 10 == 0:
+                                print('counter: ', counter, 'gloss:', gloss, 'dloss:', dloss,
+                                      'last_Dscore:', last_Dscore)
+
+                    total_img = gstep * cf.MINIBATCHSIZE
+                    current_epoch = total_img / cf.TRAIN_DATA_NUMBER
+
+                    # 一定回数ごとに現状を表示
+                    if gstep % 10 == 0:
+                        logger.info('Epoch: %.3f, total_img: %s, Dscore: %s, dloss: %s, gloss: %s, qsize: %s', current_epoch, total_img, last_Dscore, dloss, gloss, self.trainq.qsize())
+                        try:
+                            self.summarywriter.flush()
+                        except:
+                            traceback.print_exc()
+                    # 一定回数ごとにチェックポイントをセーブ
+                    if gstep % 500 == 0:
+                        try:
+                            self.saver.save(self.session,
+                                            os.path.join(self.chkpdir,'model.ckpt'),
+                                            gstep)
+                            logger.info('checkpointをセーブしました。')
+                            summary = self.session.run(merged, feed_dict=fd)
+                            self.summarywriter.add_summary(summary, gstep)
+                        except:
+                            traceback.print_exc()
+
+                    # 一定回数ごとにサンプル画像を保存
+                    if gstep % cf.SAVE_SAMPLE_MINIBATCH == 0:
+                        self.save_sample_img(self.session, fd, total_img)
+                        logger.info('サンプルを保存しました。')
+                    # 一定回数ごとに正解画像を保存
+                    if gstep % 1000 ==0:
+                        self.save_sample_img(self.session, fd, 'D_'+str(total_img), False)
+                    # 最大epochに到達したら終了フラグを立てる
+                    if current_epoch >= cf.MAXEPOCH:
+                        logger.info('max_epochに到達したため終了します。')
+                        finished = True
+        except:
+            traceback.print_exc()
+            self.training = False
+        finally:
+            logger.info('train_loopを終了します。')
+
+
+logger.info('DCGAN 学習プログラムを開始します。')
+
+# 乱数を初期化
+logger.info('乱数を初期化します')
+random.seed()
+
+# ネットワークのインスタンスを作成
+global_step_tensor = tf.train.create_global_step()
+
+D = d.Discriminator(global_step_tensor)
+G = g.Generator(D, global_step_tensor)
+
+T = Trainer(global_step_tensor, D, G)
+
+logger.info('nn.trainを開始します。')
+T.train(nqueuethreads=3)
+
+logger.info('学習が終了しました。')
+chkpdir = os.path.join(cf.RESULTDIR, cf.DESC)
+chkp = tf.train.latest_checkpoint(chkpdir)
+if chkpdir is not None and chkp is not None:
+    logger.info('最新チェックポイント: ' + tf.train.latest_checkpoint(chkpdir))
+logger.info('プログラムを終了します。')
+
